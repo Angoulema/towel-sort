893faede3a3388328069b3e7ad43c1baf12a2f42
angoulema <eugenia.labokha@yandex.by>
2020-10-05 22:40:30 +0300

    feat: add partly working solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (Array.isArray(matrix) && matrix.length) {
+    let result = matrix.reduce(
+      (result, item) => result.concat(item), []
+    ); // собирает в массиве resutlt элементы исходного массива, таким образом вложенность должна раскрыться
+    return result;
+  } else return [];
+  
 }
+
+// надо, чтобы шло "змейкой", то есть вторая строка не по порядку, а в обратном порядке.
