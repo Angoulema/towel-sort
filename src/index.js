09c75ad4f020b274a3628ea40d30566c08f9a656
angoulema <eugenia.labokha@yandex.by>
2020-10-06 23:12:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@
 
 module.exports = function towelSort (matrix) {
   if (Array.isArray(matrix) && matrix.length) {
-    let result = matrix.reduce(
-      (result, item) => result.concat(item), []
-    ); // собирает в массиве resutlt элементы исходного массива, таким образом вложенность должна раскрыться
-    return result;
+    let matrixNew = [];
+    let item = [];
+    
+    for (let i = 0; i < matrix.length; i++) {
+      if (i % 2 === 0) { // если четный элемент матрицы, который надо достать и записать в новую
+        matrixNew = matrixNew.concat(matrix[i]); 
+      } else { // если нечетный элемент исходной матрицы, который надо перевернуть
+        item = matrix[i].reverse();
+        matrixNew = matrixNew.concat(item);
+      };
+    };
+    return matrixNew;
   } else return [];
   
-}
-
-// надо, чтобы шло "змейкой", то есть вторая строка не по порядку, а в обратном порядке.
+};
